e5b67f7dc54f9a77a9d77e60d65c17cd20ac44c5
erico009 <135387637+erico009@users.noreply.github.com>
2025-04-29 10:07:27 -0400

    Add files via upload

---
+++ fmouse/config.h
@@ -0,0 +1,31 @@
+// Copyright 2023 Eric (@erico009)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+
+#define PMW33XX_CPI 500
+
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP18
+#define SPI_MOSI_PIN GP19
+#define SPI_MISO_PIN GP16
+#define PMW33XX_CS_PIN GP17
+#define POINTING_DEVICE_DEBUG
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
---
+++ fmouse/mcuconf.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include_next <mcuconf.h>
+#undef STM32_SPI_USE_SPI0
+#define STM32_SPI_USE_SPI0 TRUE
